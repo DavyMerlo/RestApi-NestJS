b3130a72d16c24984e3f080f0fcd74e2e269099f
DavyMerlo <davymerlo@live.be>
2024-01-14 20:23:33 +0100

    did some fixes in addres service layer

--- src/user.address/user.address.service.ts
+++ src/user.address/user.address.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { UserAddressRepository } from './user.address.repository';
 import { addressMapper } from '../address/mapper/address.mapper';
 import { userMapper } from '../user/mapper/user.mapper';
@@ -12,14 +12,16 @@ export class UserAddressService {
     {}
 
     async addressesByUserId(userId: number){
-        const addressByUserIdDB = await this.userAddressRepository.addressesByUserId(userId);
-        const addressesToMap = addressByUserIdDB.map(item => item.address);
+        const addressesByUserIdDB = await this.userAddressRepository.addressesByUserId(userId);
+        if(!addressesByUserIdDB || addressesByUserIdDB.length === 0) throw new  NotFoundException('No addresses found associated with userId: ' + userId);
+        const addressesToMap = addressesByUserIdDB.map(item => item.address);
         return addressMapper.mapAddresses(addressesToMap);
     }
 
     async usersByAddressId(addressId: number){
-        const userByAddressIdDb = await this.userAddressRepository.usersByAddressId(addressId);
-        const userToMap = userByAddressIdDb.map(item => item.user);
+        const usersByAddressIdDb = await this.userAddressRepository.usersByAddressId(addressId);
+        if(!usersByAddressIdDb || usersByAddressIdDb.length === 0) throw new  NotFoundException('No users found associated with addressId: ' + addressId);
+        const userToMap = usersByAddressIdDb.map(item => item.user);
         return userMapper.mapUser(userToMap);
     }
 }
